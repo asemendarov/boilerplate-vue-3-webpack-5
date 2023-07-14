10930a8a007f25d622672c8e33ec2f57523cffcf
Andrey Vitalievich <andrew.semendarov@maximumtest.ru>
2023-07-14 18:31:50 +0300

    feat: add examples in home page

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="home-page">
     <div>Page: "{{ route.name }}"</div>
     <div>
       <div>Test i18n. Msg: {{ i18n.t('hello') }}</div>
@@ -13,18 +13,58 @@
         Click Me
       </button>
     </div>
+    <div>
+      <div>Test Transition Expand</div>
+      <VTransition name="expand">
+        <pre v-if="state.isShowExpandItem">
+          Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test
+          Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test
+          Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test
+          Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test
+          Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test
+        </pre>
+      </VTransition>
+      <button @click="onTestTransitionExpandBtnClick">
+        Click Me
+      </button>
+    </div>
+    <div>
+      <div>Test Transition Group Expand</div>
+      <VTransitionGroup name="expand">
+        <li
+          v-for="item in state.expandList"
+          :key="item"
+        >
+          {{ item }}
+        </li>
+      </VTransitionGroup>
+      <button @click="onTestTransitionGroupExpandBtnClick">
+        Click Me
+      </button>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { reactive } from 'vue';
 import { useRoute } from 'vue-router';
 import { useExampleStore } from '@/store/examples';
 import { changeLanguage, LanguageCode, useI18n, currentLanguageIs } from '@/i18n';
+import { VTransition, VTransitionGroup } from '@ui/VTransition';
 
+type State = {
+  isShowExpandItem: boolean;
+  expandList: string[];
+};
+
+const exampleStore = useExampleStore();
 const route = useRoute();
 const i18n = useI18n();
 
-const exampleStore = useExampleStore();
+const state = reactive<State>({
+  isShowExpandItem: false,
+  expandList: [],
+});
 
 async function onTestChangeLanguageBtnClick() {
   if (currentLanguageIs(LanguageCode.RU)) {
@@ -37,4 +77,20 @@ async function onTestChangeLanguageBtnClick() {
 async function onTestApiBtnClick() {
   await exampleStore.loadExampleList();
 }
+
+function onTestTransitionExpandBtnClick() {
+  state.isShowExpandItem = !state.isShowExpandItem;
+}
+
+function onTestTransitionGroupExpandBtnClick() {
+  state.expandList.push(`item ${state.expandList.length}`);
+}
 </script>
+
+<style lang="scss">
+.home-page {
+  & > div {
+    margin-bottom: 24px;
+  }
+}
+</style>
