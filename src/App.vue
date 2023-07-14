de66a340d7a4b641ff5ef307fc20cc648e5d19f5
Andrey Vitalievich <andrew.semendarov@maximumtest.ru>
2023-07-14 18:31:06 +0300

    feat: add app layouts

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,37 @@
 <template>
-  <div class="content">
-    <RouterView />
+  <div class="app">
+    <Component :is="dynamicComponent">
+      <RouterView />
+    </Component>
   </div>
 </template>
+
+<script lang="ts" setup>
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+import { layoutMatcher, LayoutName } from '@/layouts';
+import VLoader from '@ui/VLoader';
+
+const route = useRoute();
+
+const isEmptyRoute = computed(() => !route.name);
+
+const firstRouteWithLayout = computed(() => route.matched.find((value) => value.meta.layout));
+
+const layoutDynamicComponent = computed(() => {
+  const layoutName = firstRouteWithLayout.value?.meta.layout ?? LayoutName.DEFAULT;
+
+  return layoutMatcher[layoutName];
+});
+
+const dynamicComponent = computed(() => {
+  if (isEmptyRoute.value) {
+    return VLoader;
+  }
+
+  return layoutDynamicComponent.value;
+});
+</script>
 
 <style lang="scss">
 @import '@/assets/scss/reset.scss';
@@ -13,16 +42,11 @@ html {
 
 body {
   font-size: 20px;
-
   width: 100%;
   height: 100%;
 }
-</style>
 
-<style lang="scss" scoped>
-.content {
-  flex-grow: 1;
-  overflow: auto;
-  padding: 10px;
+.app > .v-loader {
+  min-height: 100dvh;
 }
 </style>
